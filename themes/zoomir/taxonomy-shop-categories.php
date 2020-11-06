64464655007d08717f0a405730184c3fc0443da2
Mihail <sidorov.mihail.maykop@mail.ru>
2020-11-06 17:32:45 +0300

    Сделал вывод заглушки страницы магазины через движок.

---
+++ themes/zoomir/taxonomy-shop-categories.php
@@ -0,0 +1,160 @@
+<?php get_header(); ?>
+    <div class="shops-page section">
+        <div class="shops-page__title-desc-wrapper">
+            <div class="shops-page__title-desc">
+                <div class="shops-page__title page-title">Магазины</div>
+                <div class="shops-page__desc">
+                    Наша сеть насчитывает порядка 12 магазинов, как в городе Майкопе, так и в городе Белореченск. В этом разделе по ним собрана вся информация.
+                </div>
+            </div>
+        </div>
+
+        <div class="shops-page__map">
+            <img src="<?php echo get_template_directory_uri() ?>/resources/imgs/map.png">
+        </div>
+
+        <div class="shops-page__tabs">
+            <a href="#" class="shops-page__tab shops-page__tab_active">Майкоп</a>
+            <a href="#" class="shops-page__tab">Белореченск</a>
+        </div>
+
+        <div class="shops-page__items">
+            <a href="#" class="shops-page__item">
+                <div class="shops-page__item-title">ул. 7 Переулок 2а</div>
+                <div class="shops-page__item-image">
+                    <img src="<?php echo get_template_directory_uri() ?>/resources/imgs/shops-page-item-image1.png">
+                </div>
+                <div class="shops-page__item-text">
+                    <div class="shops-page__item-text-title">ул. 7 Переулок 2а</div>
+                    <div class="shops-page__item-text-table">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Режим работы:</td>
+                                    <td>с 9:00 до 20:00</td>
+                                </tr>
+                                <tr>
+                                    <td>Телефон:</td>
+                                    <td>89618279861</td>
+                                </tr>
+                                <tr>
+                                    <td>Отделы:</td>
+                                    <td>
+                                        <a href="#">Ветаптека</a>
+                                        <a href="#">Корма</a>
+                                        <a href="#">Аксессуары</a>
+                                        <a href="#">Садоводство</a>
+                                        <a href="#">Рыбы и рептилии</a>
+                                        <a href="#">Животные</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="shops-page__item">
+                <div class="shops-page__item-title">ул. 7 Переулок 2а</div>
+                <div class="shops-page__item-image">
+                    <img src="<?php echo get_template_directory_uri() ?>/resources/imgs/shops-page-item-image2.png">
+                </div>
+                <div class="shops-page__item-text">
+                    <div class="shops-page__item-text-title">ул. 7 Переулок 2а</div>
+                    <div class="shops-page__item-text-table">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Режим работы:</td>
+                                    <td>с 9:00 до 20:00</td>
+                                </tr>
+                                <tr>
+                                    <td>Телефон:</td>
+                                    <td>89618279861</td>
+                                </tr>
+                                <tr>
+                                    <td>Отделы:</td>
+                                    <td>
+                                        <a href="#">Ветаптека</a>
+                                        <a href="#">Корма</a>
+                                        <a href="#">Аксессуары</a>
+                                        <a href="#">Садоводство</a>
+                                        <a href="#">Рыбы и рептилии</a>
+                                        <a href="#">Животные</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="shops-page__item">
+                <div class="shops-page__item-title">ул. 7 Переулок 2а</div>
+                <div class="shops-page__item-image">
+                    <img src="<?php echo get_template_directory_uri() ?>/resources/imgs/shops-page-item-image1.png">
+                </div>
+                <div class="shops-page__item-text">
+                    <div class="shops-page__item-text-title">ул. 7 Переулок 2а</div>
+                    <div class="shops-page__item-text-table">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Режим работы:</td>
+                                    <td>с 9:00 до 20:00</td>
+                                </tr>
+                                <tr>
+                                    <td>Телефон:</td>
+                                    <td>89618279861</td>
+                                </tr>
+                                <tr>
+                                    <td>Отделы:</td>
+                                    <td>
+                                        <a href="#">Ветаптека</a>
+                                        <a href="#">Корма</a>
+                                        <a href="#">Аксессуары</a>
+                                        <a href="#">Садоводство</a>
+                                        <a href="#">Рыбы и рептилии</a>
+                                        <a href="#">Животные</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="shops-page__item">
+                <div class="shops-page__item-title">ул. 7 Переулок 2а</div>
+                <div class="shops-page__item-image">
+                    <img src="<?php echo get_template_directory_uri() ?>/resources/imgs/shops-page-item-image2.png">
+                </div>
+                <div class="shops-page__item-text">
+                    <div class="shops-page__item-text-title">ул. 7 Переулок 2а</div>
+                    <div class="shops-page__item-text-table">
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>Режим работы:</td>
+                                    <td>с 9:00 до 20:00</td>
+                                </tr>
+                                <tr>
+                                    <td>Телефон:</td>
+                                    <td>89618279861</td>
+                                </tr>
+                                <tr>
+                                    <td>Отделы:</td>
+                                    <td>
+                                        <a href="#">Ветаптека</a>
+                                        <a href="#">Корма</a>
+                                        <a href="#">Аксессуары</a>
+                                        <a href="#">Садоводство</a>
+                                        <a href="#">Рыбы и рептилии</a>
+                                        <a href="#">Животные</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+<?php get_footer(); ?>
