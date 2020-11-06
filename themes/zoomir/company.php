18acfde978ba663e3f9f9c807f74b1b627a41c23
Mihail <sidorov.mihail.maykop@mail.ru>
2020-11-06 17:19:13 +0300

    Сделал вывод статической страницы компания через движок.

---
+++ themes/zoomir/company.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Template Name: Компания
+Template Post Type: page
+*/
+?>
+<?php get_header(); ?>
+    <div class="company-page">
+        <div class="company-page__title page-title section">Компания</div>
+        <div class="company-page__inform section">
+            <div class="company-page__inform-nums">
+                <div class="company-page__inform-nums-item">
+                    <span>1997</span> Год основания
+                </div>
+                <div class="company-page__inform-nums-item">
+                    <span>9 магазинов</span> в Майкопе
+                </div>
+                <div class="company-page__inform-nums-item">
+                    <span>3 магазина</span> в Белореченске
+                </div>
+            </div>
+            <div class="company-page__inform-image">
+                <img src="<?php echo get_template_directory_uri() ?>/resources/imgs/company-page-inform-image.png">
+            </div>
+            <div class="company-page__inform-text-wrapper">
+                <div class="company-page__inform-text">
+                    <p>
+                        Развитие нашей компании началось 13 лет назад и тогда это был маленький магазинчик Зоомир. Тогда мы и подумать не могли, а только мечтали, что вырастем в большую сеть.
+                    </p>
+                    <p>
+                        И вот сейчас оглядываясь назад мы еще сильнее начинаем ценить наших покупателей, ведь все что мы делаем это именно для вас, вас владельцем своих любимых питомцев.
+                    </p>
+                    <p>
+                        Поэтому в наших магазинах товары от проверенных и надежных производителей, широкий ассортимент, чтобы вы нашли все что нужно и для любого из своих питомцев.
+                    </p>
+                    <p>
+                        Мы стремиммся и дальше улучшать свое качество и регулярно расширяем ассортимент наших магазинов, пополняя его новинками.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php get_footer(); ?>
