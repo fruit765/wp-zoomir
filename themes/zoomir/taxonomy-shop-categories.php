192a27577eb99a7c020ac20cb3f46adcb856acd8
Mihail <sidorov.mihail.maykop@mail.ru>
2020-11-09 10:51:30 +0300

    Сделал вывод магазинов на странице магазинов через движок. Сделал переключение магазинов через плашку майкоп\белореченск. Подправил отображение списка магазинов через стили. Сделал бекап БД.

--- themes/zoomir/taxonomy-shop-categories.php
+++ themes/zoomir/taxonomy-shop-categories.php
@@ -14,147 +14,26 @@
         </div>
 
         <div class="shops-page__tabs">
-            <a href="#" class="shops-page__tab shops-page__tab_active">Майкоп</a>
-            <a href="#" class="shops-page__tab">Белореченск</a>
+            <a href="/shop-categories/majkop" class="shops-page__tab<?php if (get_queried_object()->name === 'Майкоп') echo ' shops-page__tab_active'; ?>">Майкоп</a>
+            <a href="/shop-categories/belorechensk" class="shops-page__tab<?php if (get_queried_object()->name === 'Белореченск') echo ' shops-page__tab_active'; ?>">Белореченск</a>
         </div>
 
         <div class="shops-page__items">
-            <a href="#" class="shops-page__item">
-                <div class="shops-page__item-title">ул. 7 Переулок 2а</div>
-                <div class="shops-page__item-image">
-                    <img src="<?php echo get_template_directory_uri() ?>/resources/imgs/shops-page-item-image1.png">
-                </div>
-                <div class="shops-page__item-text">
-                    <div class="shops-page__item-text-title">ул. 7 Переулок 2а</div>
-                    <div class="shops-page__item-text-table">
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>Режим работы:</td>
-                                    <td>с 9:00 до 20:00</td>
-                                </tr>
-                                <tr>
-                                    <td>Телефон:</td>
-                                    <td>89618279861</td>
-                                </tr>
-                                <tr>
-                                    <td>Отделы:</td>
-                                    <td>
-                                        <a href="#">Ветаптека</a>
-                                        <a href="#">Корма</a>
-                                        <a href="#">Аксессуары</a>
-                                        <a href="#">Садоводство</a>
-                                        <a href="#">Рыбы и рептилии</a>
-                                        <a href="#">Животные</a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+            <?php $loop = new WP_Query(array('posts_per_page' => 0, 'post_type' => 'shop', 'tax_query' => array(array('taxonomy' => 'shop-categories', 'terms'=> get_queried_object()->term_id)), 'orderby' => 'id', 'order' => 'DESC')); ?>
+            <?php while ($loop->have_posts()) { $loop->the_post() ?>
+                <a href="#" class="shops-page__item">
+                    <div class="shops-page__item-title"><?php the_title(); ?></div>
+                    <div class="shops-page__item-image">
+                        <img src="<?php echo wp_get_attachment_image_url(get_post_meta(get_the_ID(), '_thumbnail_id', true), 'full'); ?>">
                     </div>
-                </div>
-            </a>
-            <a href="#" class="shops-page__item">
-                <div class="shops-page__item-title">ул. 7 Переулок 2а</div>
-                <div class="shops-page__item-image">
-                    <img src="<?php echo get_template_directory_uri() ?>/resources/imgs/shops-page-item-image2.png">
-                </div>
-                <div class="shops-page__item-text">
-                    <div class="shops-page__item-text-title">ул. 7 Переулок 2а</div>
-                    <div class="shops-page__item-text-table">
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>Режим работы:</td>
-                                    <td>с 9:00 до 20:00</td>
-                                </tr>
-                                <tr>
-                                    <td>Телефон:</td>
-                                    <td>89618279861</td>
-                                </tr>
-                                <tr>
-                                    <td>Отделы:</td>
-                                    <td>
-                                        <a href="#">Ветаптека</a>
-                                        <a href="#">Корма</a>
-                                        <a href="#">Аксессуары</a>
-                                        <a href="#">Садоводство</a>
-                                        <a href="#">Рыбы и рептилии</a>
-                                        <a href="#">Животные</a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                    <div class="shops-page__item-text">
+                        <div class="shops-page__item-text-title"><?php the_title(); ?></div>
+                        <div class="shops-page__item-text-table">
+                            <?php the_content(); ?>
+                        </div>
                     </div>
-                </div>
-            </a>
-            <a href="#" class="shops-page__item">
-                <div class="shops-page__item-title">ул. 7 Переулок 2а</div>
-                <div class="shops-page__item-image">
-                    <img src="<?php echo get_template_directory_uri() ?>/resources/imgs/shops-page-item-image1.png">
-                </div>
-                <div class="shops-page__item-text">
-                    <div class="shops-page__item-text-title">ул. 7 Переулок 2а</div>
-                    <div class="shops-page__item-text-table">
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>Режим работы:</td>
-                                    <td>с 9:00 до 20:00</td>
-                                </tr>
-                                <tr>
-                                    <td>Телефон:</td>
-                                    <td>89618279861</td>
-                                </tr>
-                                <tr>
-                                    <td>Отделы:</td>
-                                    <td>
-                                        <a href="#">Ветаптека</a>
-                                        <a href="#">Корма</a>
-                                        <a href="#">Аксессуары</a>
-                                        <a href="#">Садоводство</a>
-                                        <a href="#">Рыбы и рептилии</a>
-                                        <a href="#">Животные</a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </a>
-            <a href="#" class="shops-page__item">
-                <div class="shops-page__item-title">ул. 7 Переулок 2а</div>
-                <div class="shops-page__item-image">
-                    <img src="<?php echo get_template_directory_uri() ?>/resources/imgs/shops-page-item-image2.png">
-                </div>
-                <div class="shops-page__item-text">
-                    <div class="shops-page__item-text-title">ул. 7 Переулок 2а</div>
-                    <div class="shops-page__item-text-table">
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>Режим работы:</td>
-                                    <td>с 9:00 до 20:00</td>
-                                </tr>
-                                <tr>
-                                    <td>Телефон:</td>
-                                    <td>89618279861</td>
-                                </tr>
-                                <tr>
-                                    <td>Отделы:</td>
-                                    <td>
-                                        <a href="#">Ветаптека</a>
-                                        <a href="#">Корма</a>
-                                        <a href="#">Аксессуары</a>
-                                        <a href="#">Садоводство</a>
-                                        <a href="#">Рыбы и рептилии</a>
-                                        <a href="#">Животные</a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </a>
+                </a>
+            <?php } wp_reset_query(); ?>
         </div>
     </div>
 <?php get_footer(); ?>
