c4d60c82146c6d3c249508a83f0a769053147077
Mihail <sidorov.mihail.maykop@mail.ru>
2020-11-13 10:20:11 +0300

    Прописал иконку на вкладке.

--- themes/zoomir/header.php
+++ themes/zoomir/header.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Зоомир</title>
+        <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/resources/imgs/favicon.ico" />
         <?php wp_head(); ?>
     </head>
     <body>
